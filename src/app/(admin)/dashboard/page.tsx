4c18a389d3df8d4ddfc8c84dcba828de15acf3f0
artanton <anton.kononov.dev@gmail.com>
2025-03-29 18:44:37 +0200

    sidebar

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// export interface PageProps {}
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+    </main>
+  );
+}
