c38031ce567214d9957c1524043378da4e59a3e9
artanton <anton.kononov.dev@gmail.com>
2025-04-05 22:58:05 +0300

    companiies page slots, default, new company page innercepting route

---
+++ src/app/(admin)/companies/@header/new/page.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import Header from '@/app/components/header';
+
+
+
+export default function Page() {
+  return <Header>Add new company</Header>;
+}
---
+++ src/app/(admin)/companies/@header/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '@/app/components/header';
+
+
+
+export default function Page() {
+  return <Header>Companies</Header>;
+}
