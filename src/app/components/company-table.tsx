191175783969c385bf816be71d1d08e4150fe75d
artanton <anton.kononov.dev@gmail.com>
2025-04-15 23:31:23 +0300

    add using rect query

--- src/app/components/company-table.tsx
+++ src/app/components/company-table.tsx
@@ -1,9 +1,11 @@
+'use client';
 
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getCompanies } from '@/lib/api';
+import CompanyRow from '@/app/components/company-row';
 
-export interface CompanyTableProps {
-  children?: React.ReactNode;
-}
+// export interface CompanyTableProps {}
 
 const headers = [
   'Category',
@@ -14,7 +16,13 @@ const headers = [
   'Joined date',
 ];
 
-export default function CompanyTable({ children }: CompanyTableProps) {
+export default function CompanyTable() {
+  const { data } = useQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies(),
+    staleTime: 10 * 1000,
+  });
+
   return (
     <div className="py-8 px-10 bg-gray-100">
       <table className="table-auto w-full border-separate border-spacing-y-2">
@@ -27,7 +35,11 @@ export default function CompanyTable({ children }: CompanyTableProps) {
             ))}
           </tr>
         </thead>
-        <tbody>{children}</tbody>
+        <tbody>
+          {data?.map((company) => (
+            <CompanyRow key={company.id} company={company} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
